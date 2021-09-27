3c492830d6bfc59ed105a8282e552250860ce4d4
Alina Ger <a.gerash@bk.ru>
2021-09-28 03:01:06 +0400

    Добавила выделение ячеек с багом

--- src/App.js
+++ src/App.js
@@ -1,22 +1,106 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
+const isCellSelected = (x, y, cellSelectFrom, cellSelectTo) => {
+  let { x: xFrom, y: yFrom } = cellSelectFrom;
+  let { x: xTo, y: yTo } = cellSelectTo;
+  let temp;
+
+  if (xFrom === xTo && xFrom === x) {
+    if (yFrom > yTo) {
+      // vertical word
+      temp = yFrom;
+      yFrom = yTo;
+      yTo = temp;
+    }
+
+    if (y >= yFrom && y <= yTo) {
+      return true;
+    }
+  } else if (yFrom === yTo && yFrom === y) {
+    //horizontal word
+    if (xFrom > xTo) {
+      temp = xFrom;
+      xFrom = xTo;
+      xTo = temp;
+    }
+
+    if (x >= xFrom && x <= xTo) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 function App() {
+  const [mouseIsPressed, setMouseIsPressed] = useState(false);
+
+  const [cellSelectFrom, setCellSelectFrom] = useState({ x: -1, y: -1 });
+  const [cellSelectTo, setCellSelectTo] = useState({ x: -1, y: -1 });
+
+  const onMouseDownHandle = (event) => {
+    setMouseIsPressed(true);
+
+    setCellSelectFrom({
+      x: +event.target.attributes.x.value,
+      y: +event.target.attributes.y.value,
+    });
+
+    setCellSelectTo({
+      x: +event.target.attributes.x.value,
+      y: +event.target.attributes.y.value,
+    });
+  };
+
+  const onMouseOverHandle = (event) => {
+    if (mouseIsPressed) {
+      setCellSelectTo({
+        x: +event.target.attributes.x.value,
+        y: +event.target.attributes.y.value,
+      });
+    }
+  };
+
+  const onMouseUpHandle = (event) => {
+    setMouseIsPressed(false);
+  };
+
+  useEffect(() => {
+    console.log('cell select from', cellSelectFrom);
+  }, [cellSelectFrom]);
+
+  useEffect(() => {
+    console.log('cell select to', cellSelectTo);
+  }, [cellSelectTo]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <table>
+          <tbody>
+            {[...Array(3)].map((elem, y) => (
+              <tr key={y}>
+                {[...Array(3)].map((elem, x) => (
+                  <td
+                    key={x}
+                    x={x}
+                    y={y}
+                    className={
+                      isCellSelected(x, y, cellSelectFrom, cellSelectTo)
+                        ? 'cell_selected'
+                        : 'cell'
+                    }
+                    onMouseDown={onMouseDownHandle}
+                    onMouseOver={onMouseOverHandle}
+                    onMouseUp={onMouseUpHandle}
+                  ></td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </header>
     </div>
   );

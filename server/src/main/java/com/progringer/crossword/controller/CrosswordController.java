764fc3dcd52293b0db7a081c9c651c0e43337395
IrinaProkofieva <athos_holmes@mail.ru>
2021-12-20 15:50:20 +0400

    Выдача справочной информации

--- server/src/main/java/com/progringer/crossword/controller/CrosswordController.java
+++ server/src/main/java/com/progringer/crossword/controller/CrosswordController.java
@@ -17,7 +17,11 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -119,6 +123,16 @@ public class CrosswordController {
         dictionary.setName(name);
         fileService.saveDictionaryToFile(dictionary);
         return new DictionarySavedResponse(name);
+    }
+
+    @Operation(summary = "Получить файл справки")
+    @GetMapping("/get_info")
+    public ResponseEntity<Resource> getInfo() throws IOException {
+        ByteArrayResource resource = fileService.getInfoFile();
+        return ResponseEntity.ok()
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
     private CrosswordDto convertToCrosswordDto(Crossword crossword){

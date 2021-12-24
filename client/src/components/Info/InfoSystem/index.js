c96cc86c0e70c870a3f982671626674e62fcbb8c
llviktoriall <victoria_19.01.2000@mail.ru>
2021-12-24 18:01:40 +0400

    Попытка открыть файл о системе (Ирин)

--- client/src/components/Info/InfoSystem/index.js
+++ client/src/components/Info/InfoSystem/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const InfoSystem = () => {
+
+const [isUploaded, setIsUploaded] = useState(false);
+
+  fetch(`http://localhost:8080/get_info`, {
+        method: 'GET'
+      })
+        .then((response) => {
+          if (response.ok) {
+            setIsUploaded(true);
+          }
+          return response.json();
+          }).then((data) =>{
+                      console.log(data);
+                      document.getElementById("demo").innerHTML = data ;
+                    })
+
+        .catch((error) => {
+          console.log('error: ', error.message);
+        });
   return (
-	<section>
-      <p>Тут будут сведения о системе</p>
-    </section>
-    //вставка файла справки с сервера
-  );
+  	<section>
+        <p>Тут будут сведения о системе</p>
+        <p id="demo"></p>
+      </section>
+
+      //вставка файла справки с сервера
+    );
 };
 
 export { InfoSystem };

5978eddf92e0821be9a3e4a7892258a6bbdd6970
llviktoriall <victoria_19.01.2000@mail.ru>
2021-12-26 11:32:24 +0400

    Кноки перехода Кроссворды, Словари у админа.

--- client/src/components/AdminPanel/index.js
+++ client/src/components/AdminPanel/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AdminPanel = () => {
   return (
     <section className='welcome'>
       <p>Добро пожаловать на страницу администратора!</p>
 
-      <p>Составляйте новые кроссворды:</p>
-      <p>Создавайте новые словари:</p>
+      <p>Составляйте новые кроссворды:  <Link to='/crosswords'>Кроссворды</Link></p>
+      <p>Создавайте новые словари:  <Link to='/dictionaries'>Словари</Link></p>
     </section>
   );
 };
